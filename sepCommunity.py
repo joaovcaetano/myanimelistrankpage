7b1393e8944bb0366ebaf71e4654f046e2649125
Andre Gomes <andgomes95@gmail.com>
2018-06-23 00:30:09 -0300

    add script for save users and script for save communities

--- sepCommunity.py
+++ sepCommunity.py
@@ -8,15 +8,17 @@ from igraph import *
 pasta = "users"
 caminhos = [os.path.join(pasta,nome) for nome in os.listdir(pasta)]
 arquivos = [ar for ar in caminhos if os.path.isfile(ar)]
+
 #definir user atual
 #G = Graph(directed=False)
 vertices = []
 edges = []
 weight = []
+qtd = 30
+threshold = 0.3
 k=0
 arqUsers = []
-tabela_de_nos = []
-while(k<50):
+while(k<qtd):
 	userAtual = arquivos[k]
 	arq = pd.read_csv(userAtual, sep = '\t', header = None)
 	#leitura do user em uma tabela
@@ -24,7 +26,7 @@ while(k<50):
 	count = 0
 	tabelaFinal = []
 	tabela = []
-	if(arq[0][0] not in tabela_de_nos):
+	if(arq[0][0] not in vertices):
 		vertices.append(arq[0][i])
 		#G.add_vertices(int(arq[0][0]))
 	print "first while" + str(k)
@@ -34,11 +36,11 @@ while(k<50):
 		i = i+1
 	i = k + 1
 	#print "big while"
-	while(i<50):
+	while(i<qtd):
 		tabelaAux = []
 		arqAux = pd.read_csv(arquivos[i], sep = '\t', header = None)
 		j =0
-		if(arqAux[0][0] not in tabela_de_nos):
+		if(arqAux[0][0] not in vertices):
 			vertices.append(arqAux[0][0])
 		count = 0
 		while(j<len(arqAux[0])):#esse while monta tabela
@@ -47,7 +49,7 @@ while(k<50):
 				count = count +1
 			j = j+1
 		j = 0
-		if count > len(tabelaAux)*0.6:
+		if count > len(tabelaAux)*threshold:
 			edges.append((k,i))
 			weight.append(count)
 			#G.add_edge(int(arq[0][0]),int(arqAux[0][0]),weight = count)
@@ -58,9 +60,30 @@ while(k<50):
 print "community"
 G = Graph(vertex_attrs={"label": vertices}, edges=edges)
 comms = G.community_leading_eigenvector(clusters=10)
-print comms
-print "plot"
-#N = len(tabela_de_nos)
-#visual_style["layout"] = G.layout_fruchterman_reingold(weights=G.es["weight"], maxiter=1000, area=N**3, repulserad=N**3
-# Plot the graph
-plot(comms, mark_groups = True,layout = "fr")
+listCommun = list(comms)
+
+
+
+i = 0
+j = 0
+k = 0
+while(i<len(listCommun)):
+	base = "communities/com"+str(i)+".csv"
+	csv_modificado = open(base, "wb")
+	writer = csv.writer(csv_modificado, delimiter='\t')
+	j = 0
+	while(j<len(listCommun[i])):
+		arquivoSaida = pd.read_csv("users/"+str(vertices[listCommun[i][j]])+'.csv', sep = '\t', header = None)
+		k = 0
+		while(k < len(arquivoSaida[0])):
+			dados = []
+			dados.append(arquivoSaida[0][k])
+			dados.append(arquivoSaida[1][k])
+			dados.append(arquivoSaida[2][k])
+			writer.writerow(dados)
+			k = k+1
+		j = j+1
+	i = i+1
+
+"""print plot
+plot(comms, mark_groups = True,layout = "fr")"""
